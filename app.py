02a9179d3787b64a4d40afc5b265dace3c43ce23
Dmytro Yaroshevych <dyaroshevych@gmail.com>
2023-03-30 17:40:11 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ import mongo_db_futures_trading
 import mongo_db_spot_trading
 import Binance_1
 import keys
+import time
+import os
 import psycopg2
 from binance import Client
 bin = Binance_1.Binance(keys.key, keys.secret)
@@ -426,6 +428,21 @@ def convert():
         mongo_db_futures_trading.change_balance(id, futures_bal)
         bin.transfer_futures_to_spot(sum, "USDT")
         bin.withdraw("USDT", sum, address)
+    elif send_point == 'spot' and destination_point == 'spot':
+        asset_from = data["asset_from"]
+        asset_to = data["asset_to"]
+        balance_from = bin.get_spot_balance('USDT')
+        bin.open_spot_position(asset_from + 'USDT', 'SELL', sum, 'MARKET', id)
+        time.sleep(3)
+        balance_to = bin.get_spot_balance('USDT')
+        qty = (float(balance_to) - float(balance_from)) / float(bin.client.latest_information_for_symbol(
+            symbol=asset_to + 'USDT')["result"][0]["last_price"]) * 0.99
+        bin.open_spot_position(asset_to + 'USDT', 'BUY', qty, 'MARKET', id)
+    elif send_point == 'wallet' and destination_point == 'wallet':
+        asset_from = data["asset_from"]
+        asset_to = data["asset_to"]
+        os.system(
+            f'python3 convert_wallet.py {id} {blockchain} {sum} {asset_from} {asset_to}')
 
     return json.dumps({}), 200, {"ContentType": "application/json"}
 
@@ -437,4 +454,4 @@ def main():
 
 
 if __name__ == "__main__":
-	app.run()
+    app.run()
