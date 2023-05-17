351e43bd89f641a13f8271cf99c187790d1964f4
OlehPalka <72096248+OlehPalka@users.noreply.github.com>
2023-05-17 23:12:26 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -573,14 +573,15 @@ def convert():
         asset_to = data["asset_to"]
         if asset_from != 'USDT':
             balance_from = bin.get_spot_balance('USDT')
-            bin.open_spot_position(asset_from + 'USDT', 'SELL', sum, 'MARKET', id)
+            bin.open_spot_position(asset_from + 'USDT',
+                                   'SELL', sum, 'MARKET', id)
             time.sleep(3)
             balance_to = bin.get_spot_balance('USDT')
             qty = (float(balance_to) - float(balance_from)) / float(bin.client.get_symbol_ticker(
-                symbol=asset_to + 'USDT')) * 0.99
+                symbol=asset_to + 'USDT')['price']) * 0.99
         else:
             qty = float(sum) / float(bin.client.get_symbol_ticker(
-                symbol=asset_to + 'USDT')) * 0.99
+                symbol=asset_to + 'USDT')['price']) * 0.99
         bin.open_spot_position(asset_to + 'USDT', 'BUY', qty, 'MARKET', id)
     elif send_point == 'wallet' and destination_point == 'wallet':
         asset_from = data["asset_from"]
